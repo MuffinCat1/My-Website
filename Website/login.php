a921da3d076a2c075b6a4529c7cb5ab3dd7fabdd
Muffin Cat <matanshemaya@gmail.com>
2022-03-09 19:10:07 +0200

    Update login.php

--- Website/login.php
+++ Website/login.php
@@ -12,15 +12,16 @@ if(isset($_POST['uname']) && isset($_POST['password'])){
 
     $uname = validate($_POST['uname']);
     $pass = validate($_POST['password']);
+    $is_out = False;
 
     if(empty($uname) && empty($pass)){
-        header("Location: login_page.php?error=Please Enter the following requirments");
+        header("Location: loginPage.php?error=Please Enter the following requirments");
         exit();
     } else if(empty($uname)){
-        header("Location: login_page.php?error=User Name is required");
+        header("Location: loginpage.php?error=User Name is required");
         exit();
     } else if (empty($pass)){
-        header("Location: login_page.php?error=Password is required");
+        header("Location: loginpage.php?error=Password is required");
         exit();
     } else {
         $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
@@ -32,14 +33,16 @@ if(isset($_POST['uname']) && isset($_POST['password'])){
                 $_SESSION['user_name'] = $row['user_name'];
                 $_SESSION['name'] = $row['name'];
                 $_SESSION['id'] = $row['id'];
+                setcookie("is_out", "False", time() + 2 * 24 * 60 * 60, "index.php");
+                setcookie("user", $uname, time() + 2 * 24 * 60 * 60, "index.php");
                 header("Location: index.php?user=$uname connected succesfully");
                 exit();
             } else {
-                header("Location: login_page.php?error=Incorrect User name or password");
+                header("Location: loginPage.php?error=Incorrect User name or password");
                 exit();
             }
         } else {
-            header("Location: login_page.php?error=Incorrect User name or password");
+            header("Location: loginPage.php?error=Incorrect User name or password");
             exit();
         }
     } 
